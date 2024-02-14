13e2c8bf3aad1c5671b509992dd5c4868a740dd4
Nicholas Lombardo <nicholasmlombardo@gmail.com>
2024-02-14 12:37:37 -0800

    add falternative

--- Assets/SoundGenerator.cs
+++ Assets/SoundGenerator.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class SoundGenerator : MonoBehaviour
 {
     public List<AudioClip> clips;
+    public List<AudioClip> otherClips;
     public AudioSource audioSourcePrefab;
-
+    public Toggle soundToggle;
     public void PlayRandomFart()
     {
         AudioSource source = Instantiate(audioSourcePrefab);
-        source.clip = clips[Random.Range(0, clips.Count - 1)];
+        if (soundToggle.isOn)
+        source.clip = otherClips[Random.Range(0, otherClips.Count - 1)];
+        else
+        {
+            source.clip = clips[Random.Range(0, clips.Count - 1)];
+            
+        }
         source.Play();
         Destroy(source.gameObject, source.clip.length);
     }
